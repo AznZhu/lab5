030e1cf7eb652dadf8e56f175db48e1c6f621d2a
AznZhu <lloyd.zhu@trojans.dsu.edu>
2019-05-08 18:42:52 -0500

    Create circlething.c

---
+++ circlething.c
@@ -0,0 +1,58 @@
+#include <stdio.h> 
+#include <math.h> 
+#include "gfx2.h" 
+#include "myGfxShapeslib.h"
+
+
+// formula for the fibonacci sequence 
+int fib(int n) 
+{ 
+	if(n<=1) { 
+	return n; 
+	} else { 
+		return fib(n-1) + fib(n-2); 
+	} 
+ 
+} 
+
+ 
+int main(int argc, char * argv[]) 
+{ 
+	int ysize = 500; 
+	int xsize = 500; 
+	double radius; 
+	double XC; 
+	double YC; 
+	char temp; 
+	char c; 
+	char p; 
+	while(1) 
+	{ 
+	printf("Enter a center and a radius: "); 
+	scanf("%lf", &XC); 
+	scanf("%lf", &YC); 
+	scanf("%lf", &radius); 
+	scanf("%c", &temp); 
+	// Open a new window for drawing. 
+	gfx_open(xsize,ysize,"Circle Thing"); 
+ 
+	// Set the current drawing color to green. 
+	gfx_color(100,250,167); 
+	// draws the design using te function 
+	drawCircle(XC,YC,radius); 
+	drawcirclething(XC,YC,radius); 
+	//asks user to continue or not 
+	printf("continue? (n to quit): "); 
+  
+	scanf("%c", &p);	 
+	scanf("%c", &p); 
+	//if to decide whether to continue 
+	if (p == 'n') 
+	{ 
+	break; 
+	} 
+		c = gfx_wait(); 
+	} 
+ 
+	return 0; 
+}
